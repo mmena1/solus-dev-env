6e58133b9e04ef8cae9c373dcfc2d812c5bf108c
mrcrow85 <martinmena@outlook.com>
2017-08-18 12:59:34 -0500

    Fixed JDK script errors

--- install_jdk.sh
+++ install_jdk.sh
@@ -2,13 +2,14 @@
 #cd ~/Downloads || "Directory Downloads does not exist... Exiting..." exit
 wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.tar.gz
 if [ $? != 0 ]; then
-    tar xf jdk-8u144-linux-x64.tar.gz
+     echo "Error downloading jdk: $?"
+    exit $?
+    return
+else
+tar xf jdk-8u144-linux-x64.tar.gz
     mv jdk1.8.0_144 /opt/
     ln -svf /opt/jdk1.8.0_144/bin/java /usr/bin/java
     rm -f jdk-8u144-linux-x64.tar.gz
     exit 0
-else
-    echo "Error downloading jdk: $?"
-    exit $?
-    return
+   
 fi
